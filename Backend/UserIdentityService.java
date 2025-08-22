cbae4ffba1de23dd7d936df02ef18df8988e4632
MbizoVJ <164521750+MbizoVJ@users.noreply.github.com>
2025-08-22 10:58:40 +0200

    third
    third commit

---
+++ Backend/UserIdentityService.java
@@ -0,0 +1,40 @@
+package Backend;
+
+import java.util.List;
+
+public class UserIdentityService implements IUserIdentityService{
+    static IUserIdentityService ServiceRepo ;
+
+    public static IUserIdentityService getServiceRepo(){
+        if(ServiceRepo == null){
+            ServiceRepo = new UserIdentityService();
+        }
+
+        return ServiceRepo;
+    }
+
+    IUserIdentityRepository repository;
+
+    UserIdentityService(){
+        repository = UserIdentityRepository.createRepo();
+    }
+    @Override
+    public List<UserIdentity> getAll() {
+        return this.repository.getAll();
+    }
+
+    @Override
+    public UserIdentity create(UserIdentity t) {
+        return this.repository.create(t);
+    }
+
+    @Override
+    public UserIdentity read(String email) {
+        return this.repository.read(email);
+    }
+
+    @Override
+    public UserIdentity update(UserIdentity t) {
+        return this.repository .update(t);
+    }
+}
